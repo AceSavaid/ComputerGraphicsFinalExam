ffafb6fba7260bdd5fb1d8850239b98ee57d80fe
Ace Savaid <acetotheworld123@gmail.com>
2023-04-21 14:41:53 -0400

    updates

--- Assets/BloomHandler.cs
+++ Assets/BloomHandler.cs
@@ -8,31 +8,24 @@ public class BloomHandler : MonoBehaviour
 
     [SerializeField] Bloom bloom;
 
-    //[SerializeField] Toggle bloomToggle;
     [SerializeField][Range(1, 16)] int bloomIterations = 2;
-    //[SerializeField][Range(0, 10)] float bloomThreshold = 2.1f;
-    //[SerializeField][Range(0, 10)] float bloomIntensity = 1.4f;
+
 
     [SerializeField] Slider bloomIterationsSlider;
-    //[SerializeField] Slider bloomThresholdSlider;
-    //[SerializeField] Slider bloomIntensitySlider;
+
 
 
     void Awake()
     {
         bloomIterationsSlider.maxValue = 16;
         bloomIterationsSlider.value = bloom.iterations;
-        //bloomThresholdSlider.maxValue = 10;
-        //bloomThresholdSlider.value = bloom.threshold;
-        //bloomIntensitySlider.maxValue = 10;
-        //bloomIntensitySlider.value = bloom.intensity;
+
 
     }
 
     public void ChangeBloom()
     {
-        //bloom.intensity = bloomIntensitySlider.value;
-        //bloom.threshold = bloomThresholdSlider.value;
+
         bloom.iterations = (int)bloomIterationsSlider.value;
     }
 
--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -6,7 +6,7 @@ public class EnemyMovement : MonoBehaviour
 {
     //due to time its set bewteen points
     public Transform initposition;
-    public Transform position2;
+    public Transform endposition;
     bool inverse = false;
 
     // Start is called before the first frame update
@@ -21,15 +21,15 @@ public class EnemyMovement : MonoBehaviour
         if (inverse)
         {
             gameObject.transform.position = Vector3.MoveTowards(transform.position, initposition.position, Time.deltaTime);
-            if (Vector3.Distance(transform.position, initposition.position) < 0.4)
+            if (Vector3.Distance(transform.position, initposition.position) < 0.4f)
             {
                 inverse = true;
             }
         }
         else
         {
-            gameObject.transform.position = Vector3.MoveTowards(transform.position, position2.position, Time.deltaTime);
-            if (Vector3.Distance(transform.position, position2.position) < 0.4)
+            gameObject.transform.position = Vector3.MoveTowards(transform.position, endposition.position, Time.deltaTime);
+            if (Vector3.Distance(transform.position, endposition.position) < 0.4f)
             {
                 inverse = false;
             }
